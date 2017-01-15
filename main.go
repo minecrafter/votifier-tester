a385a3531cfdb7c4756b7b3451b9ed267147ac4a
Tux <write@imaginarycode.com>
2017-01-14 23:21:26 -0500

    Fix deployment

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "hello from go")
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
